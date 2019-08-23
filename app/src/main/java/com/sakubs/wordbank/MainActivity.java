d0aab75e7934f89f62ebe438d68d153c436a89b4
Brian Sakurada <sakuradabs@gmail.com>
2019-08-23 15:35:06 +0900

    The wordbank list is now populated into a String array.

--- app/src/main/java/com/sakubs/wordbank/MainActivity.java
+++ app/src/main/java/com/sakubs/wordbank/MainActivity.java
@@ -2,13 +2,22 @@ package com.sakubs.wordbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* The draggable words in the word bank. */
+    protected String[] wordBankList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources resources = getResources();
+        wordBankList = resources.getStringArray(R.array.nouns);
+
+
     }
 }
