85193d02a9a9ffb6d0d8e81eb6c67ae6fe597d87
Brian Sakurada <sakuradabs@gmail.com>
2019-08-23 18:03:25 +0900

    REfactored listeners into internal classes rather than implementing through the MainActivity. Changed the layout and tested with three values. The words are draggable to the sentences but there is no checking and the sentences disappear when the words are placed.

--- app/src/main/java/com/sakubs/wordbank/MainActivity.java
+++ app/src/main/java/com/sakubs/wordbank/MainActivity.java
@@ -2,25 +2,28 @@ package com.sakubs.wordbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.util.Log;
+import android.app.Activity;
+import android.content.ClipData;
+import android.graphics.Typeface;
 import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnDragListener,
-        View.OnLongClickListener {
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView option1, option2, option3, choice1, choice2, choice3;
 
     /* The draggable words in the word bank. */
     protected String[] wordBankList;
+    protected String[] sentenceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,92 +32,109 @@ public class MainActivity extends AppCompatActivity implements View.OnDragListen
 
         Resources resources = getResources();
         wordBankList = resources.getStringArray(R.array.word_bank);
+        sentenceList = resources.getStringArray(R.array.sentence_bank);
 
-        TextView word1 = (TextView) findViewById(R.id.word1);
+        //views to drag
+        option1 = (TextView)findViewById(R.id.option_1);
+        option2 = (TextView)findViewById(R.id.option_2);
+        option3 = (TextView)findViewById(R.id.option_3);
 
-        word1.setText(wordBankList[0]);
-        word1.setTag("word 1");
-        word1.setOnLongClickListener(this);
+        //views to drop onto
+        choice1 = (TextView)findViewById(R.id.choice_1);
+        choice2 = (TextView)findViewById(R.id.choice_2);
+        choice3 = (TextView)findViewById(R.id.choice_3);
 
-        findViewById(R.id.word_bank).setOnDragListener(this);
-        findViewById(R.id.word1_slot).setOnDragListener(this);
-        findViewById(R.id.word2_slot).setOnDragListener(this);
+        //set touch listeners
+        option1.setOnTouchListener(new ChoiceTouchListener());
+        option2.setOnTouchListener(new ChoiceTouchListener());
+        option3.setOnTouchListener(new ChoiceTouchListener());
+
+        //set drag listeners
+        choice1.setOnDragListener(new ChoiceDragListener());
+        choice2.setOnDragListener(new ChoiceDragListener());
+        choice3.setOnDragListener(new ChoiceDragListener());
+
+        // Set values
+        option1.setText(wordBankList[0]);
+        option2.setText(wordBankList[1]);
+        option3.setText(wordBankList[2]);
+        choice1.setText(sentenceList[0]);
+        choice2.setText(sentenceList[1]);
+        choice3.setText(sentenceList[2]);
     }
 
-    @Override
-    public boolean onDrag(View view, DragEvent dragEvent) {
-        switch (dragEvent.getAction()) {
-            case DragEvent.ACTION_DRAG_STARTED:
-                // Determines if this View can accept the dragged data
-                if (dragEvent.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                    return true;
-                }
-                return false;
+    private final class ChoiceTouchListener implements OnTouchListener {
 
-            case DragEvent.ACTION_DRAG_ENTERED:
-                // Applies a GRAY color tint to the View. Return true; the return value is ignored.
-                view.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
-                view.invalidate();
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                // Set up the drag
+                ClipData data = ClipData.newPlainText("", "");
+                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+
+                // Start dragging the item touched
+                view.startDrag(data, shadowBuilder, view, 0);
                 return true;
-
-            case DragEvent.ACTION_DRAG_LOCATION:
-                // Ignore the event
-                return true;
-
-            case DragEvent.ACTION_DRAG_EXITED:
-                view.getBackground().clearColorFilter();
-                view.invalidate();
-                return true;
-
-            case DragEvent.ACTION_DROP:
-                ClipData.Item item = dragEvent.getClipData().getItemAt(0);
-                String dragData = item.getText().toString();
-
-                Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_SHORT).show();
-                view.getBackground().clearColorFilter();
-                view.invalidate();
-
-                View vw = (View) dragEvent.getLocalState();
-                ViewGroup owner = (ViewGroup) vw.getParent();
-                owner.removeView(vw);
-
-                LinearLayout container = (LinearLayout) view;
-                container.addView(vw);
-                vw.setVisibility(View.VISIBLE);
-
-                return true;
-
-            case DragEvent.ACTION_DRAG_ENDED:
-                view.getBackground().clearColorFilter();
-                view.invalidate();
-
-                if (dragEvent.getResult())
-                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT).show();
-                return true;
-
-            default:
-                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
-                break;
+            }
+            return false;
         }
-        return false;
     }
 
-    @Override
-    public boolean onLongClick(View view) {
-        ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
-        /* Create a new ClipData using the tag as a label, the plain text MIME type, and
-         * the already-created item. This will create a new ClipDescription object within the
-         * ClipData, and set its MIME type entry to "text/plain"
-         */
-        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-        ClipData clipData = new ClipData(view.getTag().toString(), mimeTypes, item);
+    private class ChoiceDragListener implements OnDragListener {
 
-        View.DragShadowBuilder dragshadow = new View.DragShadowBuilder(view);
+        @Override
+        public boolean onDrag(View view, DragEvent dragEvent) {
+            switch (dragEvent.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DROP:
+                    //handle the dragged view being dropped over a drop view
+                    View dropView = (View) dragEvent.getLocalState();
 
-        view.startDrag(clipData, dragshadow, view, 0);
+                    //stop displaying the view where it was before it was dragged
+                    view.setVisibility(View.INVISIBLE);
 
-        return true;
+                    //view dragged item is being dropped on
+                    TextView dropTarget = (TextView) view;
+
+                    //view being dragged and dropped
+                    TextView dropped = (TextView) dropView;
+
+                    //update the text in the target view to reflect the data being dropped
+                    dropTarget.setText(dropped.getText());
+
+                    //make it bold to highlight the fact that an item has been dropped
+                    dropTarget.setTypeface(Typeface.DEFAULT_BOLD);
+
+                    //if an item has already been dropped here, there will be a tag
+                    Object tag = dropTarget.getTag();
+
+                    //if there is already an item here, set it back visible in its original place
+                    if(tag!=null) {
+                        //the tag is the view id already dropped here
+                        int existingID = (Integer)tag;
+                        //set the original view visible again
+                        findViewById(existingID).setVisibility(View.VISIBLE);
+                    }
+
+                    //set the tag in the target view to the ID of the view being dropped
+                    dropTarget.setTag(dropped.getId());
+
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //no action necessary
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
     }
 }
